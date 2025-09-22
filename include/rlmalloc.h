5bbf90393bf38fd2e3f9b2cca94e32fb19c2b3ff
Li-Richard <lirichardck@gmail.com>
2025-09-23 02:50:15 +0800

    Modified CMakeLists.txt for the program to be freestanding Deleted Makefile for CMake Created rlmalloc.h and added rl_align function Created internal.h and added abstractions for GNUC attributes

---
+++ include/rlmalloc.h
@@ -0,0 +1,36 @@
+// Richard Li memory allocator header. Created on 21th September, 2025
+#ifndef RLMALLOC_H
+#define RLMALLOC_H
+
+/* ---------------------------------Constants------------------------------- */
+#define RLMALLOC_VERSION 100 // major minor minor
+
+/* ---------------------------------Includes-------------------------------- */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdalign.h>
+#include <assert.h>
+
+#include "rlmalloc/internal.h"
+
+/* ----------------------------Function Prototyes--------------------------- */
+
+
+/* -----------------------------Inline functions---------------------------- */
+static inline void * rl_attr_always_inline rl_align(
+        void *block,
+        size_t alignment) {
+#if defined RL_DEBUG
+    assert(block != NULL);
+    assert((alignment & (alignment - 1)) == 0
+            && "Alignment is not power of 2");
+    assert(alignment <= 256 && "Alignment exceeds 256bytes");
+#endif
+
+    // no checks since this function is mostly used by the allocator,
+    // rather than the user
+    return (void *)(((uintptr_t)block + alignment - 1) & ~(alignment - 1));
+}
+
+#endif // RLMALLOC_H
+
---
+++ include/rlmalloc/internal.h
@@ -0,0 +1,35 @@
+// Internal use for rlmalloc. Created on 21th September, 2025
+#ifndef RLMALLOC_INTERNAL_H
+#define RLMALLOC_INTERNAL_H
+
+#if defined __GNUC__ || defined __clang__
+#define rl_attr_always_inline __attribute__((always_inline))
+#define rl_attr_const __attribute__((const))
+#define rl_attr_pure __attribute__((pure))
+#define rl_attr_malloc __attribute__((malloc))
+#define rl_attr_alloc_size(size) __attribute__((alloc_size(size)))
+#define rl_attr_alloc_size_2(size0, size1) __attribute__((alloc_size(size0, size1)))
+#define rl_attr_non_null __attribute__((nonnull))
+#define rl_attr_warn_unused __attribute__((warn_unused_result))
+#define rl_attr_hidden __attribute__((visibility("hidden")))
+#define rl_attr_internal __attribute__((visibility("internal")))
+#define rl_attr_constructor __attribute__((constructor))
+#define rl_attr_destructor __attribute__((destructor))
+
+#else
+#define rl_attr_always_inline
+#define rl_attr_const
+#define rl_attr_pure
+#define rl_attr_malloc
+#define rl_attr_alloc_size(size)
+#define rl_attr_alloc_size_2(size0, size1)
+#define rl_attr_non_null
+#define rl_attr_warn_unused
+#define rl_attr_hidden
+#define rl_attr_internal
+#define rl_attr_constructor
+#define rl_attr_destructor
+#endif
+
+#endif // RLMALLOC_INTERNAL_H
+
