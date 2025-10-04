3658c1a918c2b966bd1730a527265e256dbbf6cf
Li-Richard <lirichardck@gmail.com>
2025-10-04 08:52:08 +0800

    Added function prototype in error.h Added implementation to functions in error.c

--- include/rlmalloc/error.h
+++ include/rlmalloc/error.h
@@ -1,7 +1,13 @@
 #ifndef RLMALLOC_ERROR_H
 #define RLMALLOC_ERROR_H
 
+#include "internal.h"
+
+// get error message
 const char *rl_get_error(void);
+
+// set error message (for internal)
+void rl_set_error(const char *msg) rl_attr_internal;
 
 #endif // RLMALLOC_ERROR_H
 
